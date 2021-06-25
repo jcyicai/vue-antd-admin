8936f63dc40b2a1d48df233b8127e2acd2b98a81
chenyicai <chenyicai622@qq.com>
2021-06-25 17:45:00 +0800

    feat(还原): 项目还原

--- src/store/getters.js
+++
@@ -1,9 +0,0 @@
-const getters = {
-	token: (state) => state.user.token,
-	avatar: (state) => state.user.avatar,
-  introduction: (state) => state.user.introduction,
-	name: (state) => state.user.name,
-	roles: (state) => state.user.roles,
-	permission_routes: (state) => state.permission.routes
-}
-export default getters
--- src/utils/get-page-title.js
+++
@@ -1,8 +0,0 @@
-const title = 'JC Admin'
-
-export default function getPageTitle(pageTitle) {
-  if (pageTitle) {
-    return `${pageTitle} - ${title}`
-  }
-  return `${title}`
-}
